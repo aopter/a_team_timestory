6d33a33dd2c92166be1fb856ce6a3b70a8cea987
ding <1941423895@qq.com>
2021-05-14 10:07:48 +0800

    修改登录界面与全景图

--- project/TimeStory/entry/src/main/java/com/example/timesequence/ability/dynasty/slice/AllScreenImgAbilitySlice.java
+++ project/TimeStory/entry/src/main/java/com/example/timesequence/ability/dynasty/slice/AllScreenImgAbilitySlice.java
@@ -3,6 +3,7 @@ package com.example.timesequence.ability.dynasty.slice;
 import com.example.timesequence.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.webengine.WebConfig;
 import ohos.agp.components.webengine.WebView;
 
 public class AllScreenImgAbilitySlice extends AbilitySlice {
@@ -12,7 +13,13 @@ public class AllScreenImgAbilitySlice extends AbilitySlice {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_all_screen);
         WebView allScreen = (WebView) findComponentById(ResourceTable.Id_all_screen);
-        allScreen.getWebConfig().setJavaScriptPermit(true);  // 如果网页需要使用JavaScript，增加此行；如何使用JavaScript下文有详细介绍
+        WebConfig webConfig = allScreen.getWebConfig();
+        webConfig.setSecurityMode(WebConfig.SECURITY_SELF_ADAPTIVE);
+        webConfig.setDataAbilityPermit(true);
+        webConfig.setJavaScriptPermit(true);
+        webConfig.setLoadsImagesPermit(true);
+        webConfig.setLocationPermit(true);
+        webConfig.setMediaAutoReplay(true);
         final String url = "http://webapp.vizen.cn/gugong_app_pc/index.html"; // EXAMPLE_URL由开发者自定义
         allScreen.load(url);
     }
